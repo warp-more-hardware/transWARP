e6542e4bb0981ee5e8c2c54a1d8a99f8cc8447e7
Birger Schmidt <bs-github@netgaroo.com>
2023-04-04 21:01:26 +0200

    add transWARPbuilder.sh to prepare a release

---
+++ transWARPbuilder.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -x
+
+mkdir -p bin
+
+export GOOS=darwin  ; export GOARCH=amd64  ; go build -o bin/transWARP-$GOARCH-$GOOS      transWARP.go
+export GOOS=darwin  ; export GOARCH=arm64  ; go build -o bin/transWARP-$GOARCH-$GOOS      transWARP.go
+export GOOS=linux   ; export GOARCH=amd64  ; go build -o bin/transWARP-$GOARCH-$GOOS      transWARP.go
+export GOOS=linux   ; export GOARCH=386    ; go build -o bin/transWARP-$GOARCH-$GOOS      transWARP.go
+export GOOS=linux   ; export GOARCH=arm    ; go build -o bin/transWARP-$GOARCH-$GOOS      transWARP.go
+export GOOS=linux   ; export GOARCH=arm64  ; go build -o bin/transWARP-$GOARCH-$GOOS      transWARP.go
+export GOOS=windows ; export GOARCH=amd64  ; go build -o bin/transWARP-$GOARCH-$GOOS.exe  transWARP.go
+
+cp -a build/transWARP.bin bin/
+
+cp ../warp-more-hardware-esp32-firmware/software/build/warpAC011K_firmware_2_0_12_64033399_merged.bin bin/
+
+zip -9 -o -j -r transWARP.zip bin
+
